2c6c922f494ee5ac5a418834eaaaf9e1694b3b73
Ricardo <ricardo.ignacio18@hotmail.com>
2022-01-31 18:12:28 -0300

    v1.0

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 $(document).ready(function () {
-    let valor = $('#inputValor').val();
+    
 
     $("#boton").click(() => {
-        console.log('CLICKCICKCI')
+        let valor = $('#inputValor').val();
+        
 
         $.ajax({
             url: `https://superheroapi.com/api.php/10226907496816999/${valor}`,
             type: 'GET',
             DataType:'JSON',
             success(data){
-            console.log('correcto')
+
             pintarElementos(data);
             },
             error(e){
@@ -19,13 +20,31 @@ $(document).ready(function () {
         })
     });
 
+    $('#botonRandom').click(() =>{
+
+
+        let numeroRandom = Math.ceil(Math.random() * (150 - 2) + 2);
+
+
+        $.ajax({
+            url: `https://superheroapi.com/api.php/10226907496816999/${numeroRandom}`,
+            type: 'GET',
+            DataType:'JSON',
+            success(data){
+            pintarElementos(data);
+            },
+            error(e){
+                console.log('Ocurrió un error.')
+                console.log(e)
+            }
+        })
+
+    })
+
 
     const pintarElementos = (data) => {
-        console.log('desde pintarElementos', data);
-        $('#listarPosts').html("");
 
 
-        // const dataPoints = data.map(item =>({y: item.powerstats.intelligence, x: item.powerstats.strength}));
 
         const opciones = {
             title: {text: 'Estadisticas'},
@@ -37,9 +56,9 @@ $(document).ready(function () {
             },
             data: [
                 {
-                    type: 'column',
+                    type: 'pie',
                     dataPoints:[
-                        {label: "Inteligencia", y: data.powerstats.intelligence},
+                        {label: "Inteligencia", y: parseInt(data.powerstats.intelligence)},
                         {label: "strength", y: data.powerstats.strength},
                         {label: "speed", y: data.powerstats.speed},
                         {label: "durability", y: data.powerstats.durability},
@@ -63,17 +82,18 @@ $(document).ready(function () {
         
 
         let filtroArrayconnections = Object.values(data.connections);
-        console.log(filtroArrayconnections)
 
 
-            $('#listarPosts').append(`
+
+            $('#listarPosts').html(`
             <div class="col-md-12 col-12 text-center ">
                 <div class="card">
                     <div class="card-body">
-                        <div class="card-title"> ${data.name}</div>
+                        <div class="card-title"> <h1>${data.name}</h1></div>
                         <img src="${data.image.url} ">
                         <div class="row">  
                             <div class="col-md-6 col-12">
+                            <h4>POWERSTATS</h4>
                                 <p>Inteligencia: ${data.powerstats.intelligence}</p>
                                 <p>Fuerza: ${data.powerstats.strength}</p>
                                 <p>Velocidad: ${data.powerstats.speed}</p>
@@ -119,7 +139,7 @@ $(document).ready(function () {
                                 <p>relatives: ${filtroArrayconnections[1]}</p>
                             </div>
                         </div>
-                        <p class="lead">${data.speed}</p>
+                        <p class="lead">${data.powerstats.speed}</p>
                     </div>
                 </div>
               </div>
